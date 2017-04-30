183226fa670b171a27573c1608667aa40911aa5c
Rahul Bachal <rbachal@caltech.edu>
2017-04-30 16:32:15 -0700

    Updated homepage for setup

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/pypa/sampleproject',
+    url='https://github.com/AssembleSoftware/IoTPy',
 
     # Author details
     author='AssembleSoftware',
