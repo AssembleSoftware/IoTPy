ad3c516849218fed44f1c9cb5db91de8d02a28b5
kmchandy <kmchandy@gmail.com>
2018-01-03 15:35:51 -0800

    Added shared variable that stops agent execution
    Added example of a variable, stop, that is shared by two agents. Initially stop is False. When one agent sets it to True the other agent stops execution.

--- IoTPy/tests/shared_variables_test.py
+++ IoTPy/tests/shared_variables_test.py
@@ -10,6 +10,7 @@ from stream import _no_value, _multivalue
 from check_agent_parameter_types import *
 from recent_values import recent_values
 from op import *
+from sink import sink
 
 def sort(lst):
     """
@@ -108,6 +109,61 @@ def shortest_path(D):
     
     return D
 
+def test_stop():
+    """
+    Stops a sequence of numbers.
+    Shows how shared variables can be used to stop streams.
+
+    """
+    def generate_numbers(v, state, stop):
+        """
+        This function generates the sequence 0, 1, 2, ... starting
+        with the specified initial state. The function stops execution
+        when stop becomes True.
+
+        Parameters
+        ----------
+        v: The element in the sequence, 0,1,2,.. read from the input
+           stream.
+        state: The last element of the sequence
+        stop: array of length 1. This is a shared variable of the agent.
+
+        """
+        if not stop[0]:
+            return state, state+1
+        else:
+            return _no_value, state
+
+    def call_halt(v, N, stop):
+        if v > N:
+            stop[0] = True
+
+    # stop is a variable shared by both agents that are created
+    # below. It is initially False and set to True and then remains
+    # True. 
+    stop = [False]
+    numbers = Stream('numbers')
+    # Create an agent that reads and writes the same stream: numbers.
+    # The agent executes its action when a new value appears on
+    # numbers. The action puts the next value on numbers if stop is
+    # False. The action has no effect (it is a skip operation) if stop
+    # is True.
+    map_element(
+        func=generate_numbers, in_stream=numbers,
+        out_stream=numbers, state=1, stop=stop)
+    # Create an agent that sets stop to True after it reads more than
+    # N values.
+    N = 3
+    sink(func=call_halt, in_stream=numbers, N=N, stop=stop)
+
+    scheduler = Stream.scheduler
+    # Start the computation by putting a value into the numbers stream.
+    numbers.append(0)
+    scheduler.step()
+    # The stream numbers will be 0, 1, ... up to N-1 and possibly may
+    # contain additional values. For example, if N = 3 then numbers
+    # could be 0, 1, 2 or 0, 1, 2, 3, 4, 5.
+    assert range(N) == recent_values(numbers)[:N]
 
 def test_shared_variables():
     lst = [10, 6, 8, 3, 20, 2, 23, 35]
@@ -120,11 +176,10 @@ def test_shared_variables():
     assert D == [[0, 20, 30, 21], [20, 0, 10, 1],
                  [30, 10, 0, 11], [21, 1, 11, 0]]
     print 'TEST OF SHARED VARIABLES IS SUCCESSFUL!'
-    
-                
 
 if __name__ == '__main__':
     test_shared_variables()
+    test_stop()
 
     
             
