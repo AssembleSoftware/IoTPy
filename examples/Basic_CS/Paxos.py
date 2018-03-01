b42faa482a4a5100cb6e605382db2f34b030679c
Juliette Xiong <xiong.juliette@gmail.com>
2018-03-01 13:04:04 -0800

    Added daemons to Paxos

--- examples/Basic_CS/Paxos.py
+++ examples/Basic_CS/Paxos.py
@@ -4,15 +4,14 @@ Code for Paxos using IoTPy
 """
 import os
 import random
+sys.path.append(os.path.abspath("../helper_functions"))
+sys.path.append(os.path.abspath("../core"))
+sys.path.append(os.path.abspath("../agent_types"))
 
-sys.path.append(os.path.abspath("../"))
-sys.path.append(os.path.abspath("../../IoTPy/core"))
-sys.path.append(os.path.abspath("../../IoTPy/agent_types"))
-sys.path.append(os.path.abspath("../../IoTPy/helper_functions"))
-
-from stream import Stream, _no_value
+from stream import Stream, _no_value, _multivalue
 from merge import blend
 from op import map_element
+from recent_values import recent_values
 
 # num_proposers: Number of proposers
 # num_acceptors: Number of acceptors
@@ -23,13 +22,18 @@ from op import map_element
 num_proposers = 2
 num_acceptors = 3
 majority = 1 + num_acceptors/2
-proposer_out_streams = [Stream('proposer_out_'+str(i)) for i in range(num_proposers)]
-acceptor_out_streams = [Stream('acceptor_out_'+str(i)) for i in range(num_acceptors)]
+proposer_out_streams = [
+    Stream('proposer_out_'+str(i)) for i in range(num_proposers)]
+acceptor_out_streams = [
+    Stream('acceptor_out_'+str(i)) for i in range(num_acceptors)]
 time_step_stream = Stream('time_step')
 in_streams_for_each_proposer = acceptor_out_streams
 in_streams_for_each_proposer.append(time_step_stream)
-in_streams_for_each_acceptor = proposer_out_streams
 in_streams_for_time_step = in_streams_for_each_proposer
+daemons_acceptors_out_streams = [
+    Stream('daemons_acceptor_out_'+str(i)) for i in range(num_acceptors)]
+daemons_proposers_out_streams = [
+    Stream('daemons_proposer_out_'+str(i)) for i in range(num_proposers)]
 
 def random_number():
     """
@@ -38,8 +42,28 @@ def random_number():
     """
     return random.randint(1, 10)
 
+def delete(v, deletion_probability=0.1, duplication_probability=0.1):
+    random_number = random.random()
+    # Delete message
+    if random_number < deletion_probability:
+        return _no_value
+    # Duplicate message
+    elif (
+            random_number <
+            deletion_probability + duplication_probability
+            ):
+        return _multivalue([v, v])
+    # Leave message as is
+    else:
+        return v
+
+
 def proposer_behavior(input_msg, state):
+    print 'proposer. input_msg is ', input_msg
     my_id, my_prepare_timestamp, time_to_next_prepare_msg, dict_of_acceptors = state
+    print 'proposer id is', my_id
+    print 'prepare timestamp is', my_prepare_timestamp
+    print 'time remaining to next prepare message is ', time_to_next_prepare_msg
     # my_id is the id of this proposer
     # my_prepare_timestamp is the time of last prepare message
     # sent by this proposer.
@@ -106,10 +130,16 @@ def proposer_behavior(input_msg, state):
 def acceptor_behavior(input_msg, state):
     my_id, my_prepare_timestamp, prepare_proposer, \
       accept_timestamp, accept_proposer = state
+    
     msg_name, msg_timestamp, msg_proposer = input_msg
+    # If the timestamp is greater than the formerly accepted timestamp or 
+    # if it's equal, then if the current proposer has a higher number than 
+    # the formerly accepted proposal, we prepare or accept the message
     if (msg_timestamp > my_prepare_timestamp or
         (msg_timestamp == my_prepare_timestamp) and (msg_proposer >= prepare_proposer)
         ):
+        # If the message is prepare, then the acceptor grants it permission 
+        # and sends it back to the proposers
         if msg_name == 'prepare':
             my_prepare_timestamp, prepare_proposer = msg_timestamp, msg_proposer
             output_msg = (
@@ -120,8 +150,13 @@ def acceptor_behavior(input_msg, state):
             state = my_id, my_prepare_timestamp, prepare_proposer, \
               accept_timestamp, accept_proposer 
             return output_msg, state
+        # If the message is accept, then the acceptor accepts the message
+        # and sets the value and proposer to be the received timestamp
+        # and proposer number
         else:
             assert msg_name == 'accept'
+            # If the message has been accepted, then update the timestamp
+            # to the accepted value
             accept_timestamp, accept_proposer = msg_timestamp, msg_proposer
             state = my_id, my_prepare_timestamp, prepare_proposer, \
               accept_timestamp, accept_proposer
@@ -132,39 +167,49 @@ def acceptor_behavior(input_msg, state):
 def time_step_behavior(input_msg, state):
     msg_name = input_msg[0]
     dict_of_accepted_values, previously_learned_value = state
+    # If the message is accepted, we check for majority accepted values
     if msg_name == 'accepted':
         acceptor_id, accept_timestamp, accept_proposer = input_msg[1:]
         dict_of_accepted_values[acceptor_id] = (accept_timestamp, accept_proposer)
+        # If the number of accepted values is greater than the majority, we
+        # check to see if any one value has been accepted by the majority
+        # of acceptors
         if len(dict_of_accepted_values) >= majority:
             list_of_accepted_values = dict_of_accepted_values.values()
             for v in list_of_accepted_values:
+                # If the acceptors have reached a majority agreement on the
+                # accepted value, set the learned value to the majority 
+                # accepted value
                 if len([w == v for w in list_of_accepted_values]) >= majority:
                     learned_value = v
+                    # Print the newly learned value
                     if previously_learned_value != learned_value:
                         print 'learned_value', learned_value
                         previously_learned_value = learned_value
                         state = dict_of_accepted_values, previously_learned_value
         return _no_value, state
+    # If the message is time_step, then we add one to my_prepare_timestep 
     elif msg_name == 'time_step':
         if input_msg[1] < 100 and not previously_learned_value:
             return ('time_step', input_msg[1]+1), state
         else:
             return _no_value, state
+    # If the message is granted, then we simply return
     else:
         assert msg_name == 'granted'
         return _no_value, state
         
 def run_Paxos():
     proposers = [blend(func=proposer_behavior,
-                       in_streams=in_streams_for_each_proposer,
+                       in_streams=daemons_proposers_out_streams,
                        out_stream=proposer_out_streams[id],
                        state=(id, 0, random_number(), {}),
                        name='proposer_'+str(id)
                        )
                        for id in range(num_proposers)
                 ]
-    acceptors = [blend(func=acceptor_behavior,
-                       in_streams=in_streams_for_each_acceptor,
+    acceptors = [map_element (func=acceptor_behavior,
+                       in_stream=daemons_acceptors_out_streams[id],
                        out_stream=acceptor_out_streams[id],
                        state=(id, -1, -1, -1, -1),
                        name='acceptor_'+str(id)
@@ -179,14 +224,31 @@ def run_Paxos():
                             name='time step agent'
                             )
     
+    daemons_acceptors = [blend(
+        func=delete,
+        in_streams = proposer_out_streams,
+        out_stream = daemons_acceptors_out_streams[id],
+        name = 'daemons_acceptors_'+str(id)
+        )
+        for id in range(num_acceptors)
+        ]
+        
+    daemons_proposers = [blend(
+        func=delete,
+        in_streams = acceptor_out_streams,
+        out_stream = daemons_proposers_out_streams[id],
+        name = 'daemons_proposers_'+str(id)
+        )
+        for id in range(num_proposers)
+        ]
+        
     
     #STEP 4. START COMPUTATION
     # Get the scheduler and execute a step.
     scheduler = Stream.scheduler
     # Start the computation by putting a value into the
     # time step stream
-    time_step_stream.append(('time_step', 1))
-    # Start the scheduler.
+    time_step_stream.append(('time_step', 1))    # Start the scheduler.
     scheduler.step()
 
 if __name__ == '__main__':
