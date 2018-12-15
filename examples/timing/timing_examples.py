d133a58702759469ea2e3644b9c1bb6087afd59c
Kanianthra Mani Chandy <kmchandy@gmail.com>
2018-12-14 23:03:11 -0800

    Changes due to change in make_process

--- examples/timing/timing_examples.py
+++ examples/timing/timing_examples.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.abspath("../../IoTPy/helper_functions"))
 sys.path.append(os.path.abspath("../../IoTPy/core"))
 sys.path.append(os.path.abspath("../../IoTPy/agent_types"))
 sys.path.append(os.path.abspath("../../IoTPy/multiprocessing"))
-from source import source_function
+from source import source_func_to_stream
 from multicore import single_process_single_source
 from sink import sink_element
 
@@ -107,7 +107,9 @@ def time_and_offset(list_of_ntp_servers):
     return time.time(), 0.0
 
 
-
+#----------------------------------------------------------
+#          TESTS
+#----------------------------------------------------------
 
 ntp_server = "0.us.pool.ntp.org"
 
@@ -130,7 +132,7 @@ def h(in_stream):
 
 def test_0():
     def g(out_stream):
-        return source_function(
+        return source_func_to_stream(
             func=clock_drift_from_ntp_server,
             out_stream=out_stream,
             time_interval=0.1, num_steps=3,
@@ -140,7 +142,7 @@ def test_0():
 
 def test_1():
     def g(out_stream):
-        return source_function(
+        return source_func_to_stream(
             func=clock_drift_from_first_ntp_server,
             out_stream=out_stream,
             time_interval=0.1, num_steps=3,
@@ -151,13 +153,14 @@ def test_1():
 
 def test_2():
     def g(out_stream):
-        return source_function(
+        return source_func_to_stream(
             func=time_and_offset,
             out_stream=out_stream,
             time_interval=0.1, num_steps=3,
             list_of_ntp_servers=list_of_ntp_servers
             )
     single_process_single_source(source_func=g, compute_func=h)
+
 if __name__ == '__main__':
     print 'starting test_0'
     test_0()
