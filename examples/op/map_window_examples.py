c57b67be6cafb2c5eaf01775a544fdce8527d805
Kanianthra Mani Chandy <kmchandy@gmail.com>
2019-03-15 11:03:46 -0700

    Added docs

--- examples/op/map_window_examples.py
+++ examples/op/map_window_examples.py
@@ -21,9 +21,8 @@ from check_agent_parameter_types import *
 from recent_values import recent_values
 from op import map_window
 
-scheduler = Stream.scheduler
-# In the following, x is a stream that must be declared
-# before the functions are called.
+# In the following examples, x is a stream that is an input stream of
+# the agents. 
 x = Stream('x')
 
 #----------------------------------------------------------------    
@@ -32,61 +31,58 @@ x = Stream('x')
 s = Stream()
 map_window(func=sum, in_stream=x, out_stream=s, 
  window_size=2, step_size=10)
-# s[j] = f(x[j*step_size : j*step_size + window_size]) for all j.
-# s[j] = x[10*j] + x[10*j+1], for all j
+# out_stream[j] = \
+# func(in_stream[j*step_size : j*step_size + window_size])
+# for all j.
+# So:
+#       s[j] = x[10*j] + x[10*j+1], for all j
 
 #----------------------------------------------------------------    
 # Example with state and no additional parameters
 #----------------------------------------------------------------
 this_list = range(10)
-def comp(this_list, sum_of_previous_list):
+def comp(this_list, state):
    # Current state is sum_of_previous_list
    # Next state will be sum(this_list)
-   return sum(this_list) > sum_of_previous_list, sum(this_list)
+   # return next element of output stream, next state
+   next_state = sum(this_list)
+   next_output_element = sum(this_list) > state
+   return next_output_element, next_state
 c = Stream()
-# The initial state is 0
 map_window(func=comp, in_stream=x, out_stream=c, 
-           state=0, window_size=10, step_size=10)
+           state=0, window_size=10, step_size=20)
+# The initial state is 0
+# The initial value of this_list is x[:window_size]
+# The zeroth element of the output stream is:
+# func(x[:window_size], state)
 # c[0] = sum(x[:10]) > 0 because 0 is the initial state
-# c[j] = sum(x[10*j:10*j+10]) > sum(x[10*(j-1):10*(j-1)+10])
+# c[j] = sum(x[20*j:20*j+10]) > sum(x[20*(j-1):20*(j-1)+10])
 
 #----------------------------------------------------------------    
 # Example with no state and additional parameters
 #----------------------------------------------------------------
-def thresh(this_list, threshold):
-   return sum(this_list) > threshold
+# The additional parameter is threshold
+def thresh(in_stream_window, threshold):
+   return sum(in_stream_window) > threshold
 t = Stream()
-map_window(thresh, x, t, window_size=10, step_size=10, threshold=10)
-# For all j, : y[j] is True if and only if sum(x[2*j : 2*j+4]) > 10.
+map_window(func=thresh, in_stream=x, out_stream=t, window_size=10,
+           step_size=20, threshold=5) 
+# For all j, : t[j] is True if and only if sum(x[20*j : 20*j+10]) > 5.
+#
+# Note: You can use any names as arguments in the
+# definition of func, as in:
+# def thresh(v, s): return sum(v) > w
 
 #----------------------------------------------------------------    
 # Example with state and additional parameters
 #----------------------------------------------------------------
-def maxes(this_list, max_of_sums, alpha):
-   # The current window is this_list
-   # The current state is max_of_sums
-   # max_of_sums is the max over all windows so far
-   # of sum(window).
-   # alpha is a parameter whose value is specified in
-   # the call to map_window.
-   # The next state is the max over all past windows
-   # and the current window. So, the next state is:
-   # max(max_of_sums, sum(this_list))
-   return (sum(this_list) > alpha * max_of_sums,
-           max(max_of_sums, sum(this_list)))
+def maxes(in_stream_window, state, parameter):
+   next_output_element = sum(in_stream_window) > parameter * state
+   next_state = max(state, sum(in_stream_window))
+   return (next_output_element, next_state)
 m = Stream()
-map_window(maxes, x, m, state=0, window_size=20, step_size=10, 
-           alpha=0.5)
-
-x.extend(range(100))
-print 'x'
-print recent_values(x)
-scheduler.step()
-print 'sum'
-print recent_values(s)
-print 'comp'
-print recent_values(c)
-print 'threshold'
-print recent_values(t)
-print 'maxes'
-print recent_values(m)
+map_window(func=maxes, in_stream=x, out_stream=m, state=0,
+           window_size=20, step_size=10, parameter=0.5)
+# in_stream_window on step j is x[10*j : 10*j + 20]
+# state[j] = max(state[j-1], sum(x[10*j : 10*j + 20]))
+# m[j] = sum(x[10*j : 10*j + 20]) > 0.5 * state[j]
