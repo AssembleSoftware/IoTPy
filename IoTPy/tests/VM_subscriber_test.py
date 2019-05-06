70574060d8605b0d3151caa537e54874b8aef349
Kanianthra Mani Chandy <kmchandy@gmail.com>
2019-05-06 14:44:49 -0700

    Version 0.0

--- IoTPy/tests/VM_subscriber_test.py
+++ IoTPy/tests/VM_subscriber_test.py
@@ -23,75 +23,26 @@ sys.path.append(os.path.abspath("../agent_types"))
 
 from distributed import distributed_process
 from VM import VM
-from sink import sink_element
+from sink import sink_element, stream_to_file
 
-def single_process_publication_subscriber():
-    """
-    The application in this example consists of single process.
-    The process has no source and no actuator. It has a single
-    in_stream called 'in'.
+def single_process_subscriber():
 
-    This example creates a virtual machine (vm) which subscribes to
-    a stream called 'sequence'.
+    def compute_func(in_streams, out_streams):
+        stream_to_file(in_streams[0], 'result.dat')
 
-    The steps for creating a process are:
-    (1) Define the sources.
-        In this example we have no sources.
-    (2) Define the actuators.
-        In this example we have no actuators.
-    (3) Define compute_func. This process has a single input stream
-        and no output stream.
-    (4) Create the process by calling distributed_process()
-
-    Final step
-    After creating all processes, specify the connections between
-    processes and run the virtual machine..
-
-    """
-    # SKIP STEPS 1, 2 BECAUSE NO SOURCES OR ACTUATORS.
-
-    # STEP 3: DEFINE COMPUTE_FUNC
-    def g(in_streams, out_streams):
-        def print_element(v): print 'stream element is ', v
-        sink_element(
-            func=print_element, in_stream=in_streams[0])
-
-    # STEP 4: CREATE PROCESSES
     proc_1 = distributed_process(
-        compute_func=g,
+        compute_func=compute_func,
         in_stream_names=['in'],
         out_stream_names=[],
         connect_sources=[],
         connect_actuators=[],
         name='proc_1')
 
-    # FINAL STEP: CREATE A VM AND START IT.
-    # Since this application has a single process it has no
-    # connections between processes. The process, proc_1, subscribes
-    # to a stream called 'sequence'. This process does not publish
-    # streams. 
     vm_1 = VM(
         processes=[proc_1],
         connections=[],
-        subscribers=[(proc_1, 'in', 'sequence')])
+        subscribers=[(proc_1, 'in', 'copy_of_source_list')])
     vm_1.start()
 
-
-# ----------------------------------------------------------------
-# ----------------------------------------------------------------
-#             TESTS
-# ----------------------------------------------------------------
-# ----------------------------------------------------------------
-
-def single_process_publication_subscriber_test():
-    print
-    print 'Starting single_process_subscription_example_1()'
-    single_process_publication_subscriber()
-    print
-    print '-----------------------------------------------------'
-
-def test():
-    single_process_publication_subscriber_test()
-
 if __name__ == '__main__':
-    test()
+    single_process_subscriber()
