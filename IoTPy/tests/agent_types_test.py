924702f53ebccabb78d25645eef949897f6d3914
Kanianthra Mani Chandy <kmchandy@gmail.com>
2019-05-24 22:09:53 -0700

    Small change

--- IoTPy/tests/agent_types_test.py
+++ IoTPy/tests/agent_types_test.py
@@ -16,8 +16,8 @@ def test():
     test_multi()
     test_shared_variables()
     test_sink()
-    print 'Starting test of source. Takes few seconds.'
-    print 'This test will output "input queue empty" a few times.'
+    print ('Starting test of source. Takes few seconds.')
+    print ('This test will output "input queue empty" a few times.')
     test_source()
 
 
