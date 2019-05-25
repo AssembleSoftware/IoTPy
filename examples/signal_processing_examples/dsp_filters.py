18700e9902248def3a89131d20c813f149fe4deb
Kanianthra Mani Chandy <kmchandy@gmail.com>
2019-05-24 22:11:59 -0700

    New

--- examples/signal_processing_examples/dsp_filters.py
+++ examples/signal_processing_examples/dsp_filters.py
@@ -13,7 +13,8 @@ from op import map_window_list
 from recent_values import recent_values
 from scipy.signal import butter, filtfilt
 
-# See scipy
+#------------------------------------------------------------------
+# See scipy for bandpass filters.
 def butter_bandpass(lowcut, highcut, fs, order=5):
     nyq = 0.5 * fs
     low = lowcut / nyq
@@ -26,9 +27,31 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
     y = filtfilt(b, a, data)
     return y
 
-def butter_bandpass_stream(in_stream, out_stream, window_size, step_size, lowcut, highcut, fs, order=5):
+#-------------------------------------------------------------------------
+# Filters using second order sections (sos). See scipy.
+# sos filters give better response.
+def butter_bandpass_sos(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    sos = butter(order, [low, high], analog=False,
+                 btype='band', output='sos')
+    return sos
+ 
+def butter_bandpass_filter_sos(
+        in_stream, lowcut, highcut, fs, order=5):
+    in_stream = np.array(in_stream)
+    sos = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = sosfiltfilt(sos, in_stream)
+    return y
+
+#---------------------------------------------------------------
+# Streaming filters on arrays
+def bandpass_stream(
+        filter, in_stream, out_stream, window_size,
+        step_size, lowcut, highcut, fs, order=5):
     def f(data):
-        y = butter_bandpass_filter(data, lowcut, highcut, fs, order)
+        y = filter(data, lowcut, highcut, fs, order)
         return y[:step_size]
     map_window_list(
         f, in_stream, out_stream,
@@ -36,6 +59,17 @@ def butter_bandpass_stream(in_stream, out_stream, window_size, step_size, lowcut
         #lowcut=lowcut, highcut=highcut, fs=fs, order=order)
 
 def test():
+    """
+    This test generates waves with low, medium and high
+    frequency, and sums these three waves to get a combined
+    frequency wave. Then it puts the combined frequency
+    wave through a bandpass filter which passes only the
+    medium-frequency wave through.
+
+    The output plot has the combined frequency wave in the
+    first subplot and the filter output in the second subplot.
+
+    """
     # sr: sample rate
     sr = 10000
     # ma: maximum amplitude
@@ -47,7 +81,7 @@ def test():
     # or: order
     order = 6
     wave_data_low_frequency = generate_sine_wave(
-        frequency=16, max_amplitude=ma, phase_shift=ps,
+        frequency=8, max_amplitude=ma, phase_shift=ps,
         sample_rate=sr, time_duration=td)
     wave_data_medium_frequency = generate_sine_wave(
         frequency=256, max_amplitude=ma, phase_shift=ps,
@@ -60,10 +94,10 @@ def test():
         wave_data_medium_frequency +
         wave_data_high_frequency)
         
-
     x = StreamArray('x')
     y = StreamArray('y')
-    butter_bandpass_stream(
+    bandpass_stream(
+        filter=butter_bandpass_filter,
         in_stream=x, out_stream=y, window_size=1024, step_size=128,
         lowcut=64, highcut=512, fs=sr, order=order)
     x.extend(wave_data_combined_frequencies)
