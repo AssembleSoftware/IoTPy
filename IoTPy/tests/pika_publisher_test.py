dff14cbd5b7bf649cbcaeda958619bff8ca86fff
Kanianthra Mani Chandy <kmchandy@gmail.com>
2019-08-21 15:25:31 -0700

    Removed CloudAMQP

--- IoTPy/tests/pika_publisher_test.py
+++ IoTPy/tests/pika_publisher_test.py
@@ -1,16 +1,28 @@
+"""
+You MUST test pika_subscriber_test.py and pika_publisher_test.py
+before you use distributed computing with IoTPy.
+
+When you execute:
+                   pika_subscriber_test.py
+in one terminal window, and execute
+                   pika_publisher_test.py argument
+in a different terminal window, you shoud see the argument echoed
+in the subscriber window.
+
+Look at:
+ https://www.rabbitmq.com/tutorials/tutorial-one-python.html
+
+"""
 #!/usr/bin/env python
 import pika
 import sys
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
-
-channel.exchange_declare(exchange='logs',
-                         exchange_type='fanout')
+channel.exchange_declare(exchange='logs', exchange_type='fanout')
 
 message = ' '.join(sys.argv[1:]) or "info: Hello World!"
-channel.basic_publish(exchange='logs',
-                      routing_key='',
-                      body=message)
+channel.basic_publish(exchange='logs', routing_key='', body=message)
 print(" [x] Sent %r" % message)
+
 connection.close()
--- IoTPy/tests/pika_subscriber_test.py
+++ IoTPy/tests/pika_subscriber_test.py
@@ -1,33 +1,32 @@
-#import pika
+"""
+You MUST test pika_subscriber_test.py and pika_publisher_test.py
+before you use distributed computing with IoTPy.
+
+When you execute:
+                   pika_subscriber_test.py
+in one terminal window, and execute
+                   pika_publisher_test.py argument
+in a different terminal window, you shoud see the argument echoed
+in the subscriber window.
+
+Look at:
+ https://www.rabbitmq.com/tutorials/tutorial-one-python.html
+
+"""
 import pika, os
 
-url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost/%2f')
-params = pika.URLParameters(url)
-## parameters = pika.URLParameters('amqp://guest:guest@rabbit-server1:5672/%2F')
-params.socket_timeout = 5
-
-##connection = pika.BlockingConnection(parameters)
-connection = pika.BlockingConnection(params)
-#connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
-
-channel.exchange_declare(exchange='logs',
-                         exchange_type='fanout')
+channel.exchange_declare(exchange='logs', exchange_type='fanout')
 
 result = channel.queue_declare(exclusive=True)
 queue_name = result.method.queue
-
 channel.queue_bind(exchange='logs',
                    queue=queue_name)
 
 print(' [*] Waiting for logs. To exit press CTRL+C')
-
 def callback(ch, method, properties, body):
     print(" [x] %r" % body)
-
-channel.basic_consume(callback,
-                      queue=queue_name,
-                      no_ack=True)
+channel.basic_consume(callback, queue=queue_name, no_ack=True)
 
 channel.start_consuming()
