7a9db1fe6e0063cd1ca6649b785002ee69e167a4
Kanianthra Mani Chandy <kmchandy@gmail.com>
2020-01-19 20:42:49 -0800

    Added details of tests matching documentation in AssembleSoftware.com

--- IoTPy/tests/multicore_test.py
+++ IoTPy/tests/multicore_test.py
@@ -41,6 +41,7 @@ sys.path.append(os.path.abspath("../../examples/timing"))
 from multicore import *
 from merge import zip_stream
 from basics import map_e, map_l, map_w, merge_e
+from run import run
 
 
 @map_e
@@ -88,21 +89,78 @@ def source_thread_target(proc, stream_name):
         time.sleep(0)
     return
 
+def test_1_single_process():
+    """
+    This is a single process example which is converted into a
+    multicore example in test_1(), see below.
+
+    The partitioning to obtain multiple cores and threads is
+    done as follows.
+
+    (1) put_data_in_stream() is converted to a function which
+    is the target of a thread. In test_1() this function is
+    source_thread_target(proc, stream_name)
+
+    (2) double(x,y) is put in a separate process. The compute
+    function of this process is f(). Since the parameters
+    of compute_func are in_streams and out_streams, we get
+    f from double in the following way:
+    
+    def f(in_streams, out_streams):
+        double(in_stream=in_streams[0], out_stream=out_streams[0])
+    
+
+    (3) increment() and print_stream() are in a separate process.
+    The compute function of this process is g().
+
+    Run both test_1_single_process() and test_1() and look at
+    their identical outputs.
+
+    """
+
+    # ********************************************************
+    # We will put this function in its own thread in test_1()
+    def put_data_in_stream(stream):
+        num_steps=5
+        step_size=4
+        for i in range(num_steps):
+            data = list(range(i*step_size, (i+1)*step_size))
+            stream.extend(data)
+            run()
+        return
+
+    # ********************************************************
+    # We will put these lines in a separate process in test_1()
+    x = Stream('x')
+    y = Stream('y')
+    double(x, y)
+
+    # *********************************************************
+    # We will put these lines in a separate process in test_1().
+    s = Stream(name='s')
+    increment(y, s)
+    print_stream(s, name=s.name)
+
+    # *********************************************************
+    # This function is executed in a separate thread in test_1().
+    put_data_in_stream(x)
+    
+    
 #--------------------------------------------------------------------
 def test_1():
     # Functions wrapped by agents
     def f(in_streams, out_streams):
-        double(in_stream=in_streams[0], out_stream=out_streams[0])
+        double(in_streams[0], out_streams[0])
 
     def g(in_streams, out_streams):
         s = Stream(name='s')
-        increment(in_stream=in_streams[0], out_stream=s)
+        increment(in_streams[0], s)
         print_stream(s, name=s.name)
 
     # Specify processes and connections.
     processes = \
       {
-        'get_source_data_and_compute_process':
+        'source_process':
            {'in_stream_names_types': [('in', 'i')],
             'out_stream_names_types': [('out', 'i')],
             'compute_func': f,
@@ -125,10 +183,10 @@ def test_1():
     
     connections = \
       {
-          'get_source_data_and_compute_process' :
+          'source_process' :
             {
                 'out' : [('aggregate_and_output_process', 'in')],
-                'acceleration' : [('get_source_data_and_compute_process', 'in')]
+                'acceleration' : [('source_process', 'in')]
             },
            'aggregate_and_output_process':
             {}
@@ -143,23 +201,23 @@ def test_2():
     
     # Functions wrapped by agents
     def f(in_streams, out_streams):
-        multiply_and_add(in_stream=in_streams[0], out_stream=out_streams[0],
+        multiply_and_add(in_streams[0], out_streams[0],
                          multiplicand=2, addend=1)
 
     def g(in_streams, out_streams):
-        filter_then_square(in_stream=in_streams[0], out_stream=out_streams[0],
+        filter_then_square(in_streams[0], out_streams[0],
                            filter_threshold=20)
 
     def h(in_streams, out_streams):
         s = Stream('s')
-        sum_window(in_stream=in_streams[0], out_stream=s, window_size=3, step_size=3)
+        sum_window(in_streams[0], s, window_size=3, step_size=3)
         print_stream(s, name=s.name)
         
 
     # Specify processes and connections.
     processes = \
       {
-        'get_source_data_and_compute_process':
+        'source_process':
            {'in_stream_names_types': [('in', 'i')],
             'out_stream_names_types': [('out', 'i')],
             'compute_func': f,
@@ -189,10 +247,10 @@ def test_2():
     
     connections = \
       {
-          'get_source_data_and_compute_process' :
+          'source_process' :
             {
                 'out' : [('filter_and_square_process', 'in')],
-                'acceleration' : [('get_source_data_and_compute_process', 'in')]
+                'acceleration' : [('source_process', 'in')]
             },
            'filter_and_square_process' :
             {
@@ -210,18 +268,18 @@ def test_3():
 
     # Functions wrapped by agents
     def f(in_streams, out_streams):
-        multiply_and_add(in_stream=in_streams[0], out_stream=out_streams[0],
+        multiply_and_add(in_streams[0], out_streams[0],
                          multiplicand=2, addend=1)
 
     def g(in_streams, out_streams):
         t = Stream('t')
-        filter_then_square(in_stream=in_streams[0], out_stream=t,
+        filter_then_square(in_streams[0], t,
                            filter_threshold=20)
         print_stream(t, name='p1')
 
     def sums(in_streams, out_streams):
         s = Stream('s')
-        sum_window(in_stream=in_streams[0], out_stream=s, window_size=3, step_size=3)
+        sum_window(in_streams[0], s, window_size=3, step_size=3)
         print_stream(s, name='           p2')
 
     processes = \
@@ -274,18 +332,18 @@ def test_4():
 
     # Functions wrapped by agents
     def f(in_streams, out_streams):
-        identical(in_stream=in_streams[0], out_stream=out_streams[0])
+        identical(in_streams[0], out_streams[0])
 
     def g(in_streams, out_streams):
-        multiply(in_stream=in_streams[0], out_stream=out_streams[0],
+        multiply(in_streams[0], out_streams[0],
                  multiplicand=2)
 
     def h(in_streams, out_streams):
-        square(in_stream=in_streams[0], out_stream=out_streams[0])
+        square(in_streams[0], out_streams[0])
 
     def m(in_streams, out_streams):
         s = Stream('s')
-        sum_numbers(in_streams=in_streams, out_stream=s)
+        sum_numbers(in_streams, s)
         print_stream(s, name='s')
 
     processes = \
@@ -355,8 +413,19 @@ if __name__ == '__main__':
     print ('--------------------------------')
     print ('')
     print ('')
+    print ('start test_1_single_process')
+    print ('Output of test_1_single process() is identical:')
+    print ('to output of test_1()')
+    print ('[1, 3, 5, ..., 39]')
+    print ('')
+    test_1_single_process()
+    print ('')
+    print ('')
+    print ('--------------------------------')
+    print ('')
+    print ('')
     print ('starting test_2')
-    print ('Output of get_source_data_and_compute_process is: ')
+    print ('Output of source_process is: ')
     print ('[1, 3, 5, 7, 9, 11, .... ,39 ]')
     print ('')
     print ('Output of filter_and_square_process is:')
