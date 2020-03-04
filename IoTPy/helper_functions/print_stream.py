658eba3d71a6d751142618d2b169a84dd6dd4b63
Kanianthra Mani Chandy <kmchandy@gmail.com>
2020-03-04 15:45:24 -0800

    Added indices to stream output. eg. stream[0], stream[1], ...

--- IoTPy/helper_functions/print_stream.py
+++ IoTPy/helper_functions/print_stream.py
@@ -3,8 +3,6 @@ import os
 sys.path.append(os.path.abspath("../agent_types"))
 from sink import sink_element
 
-
-
 def print_stream(in_stream, name=None):
     """
     Creates a sink agent that prints values in in_stream.
@@ -15,7 +13,9 @@ def print_stream(in_stream, name=None):
        input stream of the sink agent.
 
     """
-    def print_output(v):
-        if name == None: print (v)
-        else: print (name + ' = ' + str(v))
-    sink_element(func=print_output, in_stream=in_stream)
+    def print_output(v, index):
+        if name == None:
+            print ('[' + str(index) + '] = ' + str(v))
+        else: print (name + '[' + str(index) + '] = ' + str(v))
+        return index+1
+    sink_element(func=print_output, in_stream=in_stream, state=0)
