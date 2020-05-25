8a1b8b2a743596635dfa0fadbe155b2a93c097bd
deeps-nars <deepak.narayanan@iitgn.ac.in>
2020-05-25 20:34:37 +0530

    Added setup file for packaging

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="iotpy", # Replace with your own username
+    version="0.0.1",
+    author="Mani Chandy",
+    author_email="kmchandy@gmail.com",
+    description="IoTPy - Python for Streams",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AssembleSoftware/IoTPy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
