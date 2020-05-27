1e769bc8efa757438e29c062d8843f4513be6b8f
deeps-nars <deepak.narayanan@iitgn.ac.in>
2020-05-27 21:52:34 +0530

    Changed setup.py and made initial installation. No issues.

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
-import setuptools
+from setuptools import find_packages, setup
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="iotpy", # Replace with your own username
+with open("requirements.txt") as f:
+    REQ = f.read().splitlines()
+
+
+CLASSIFIERS = ['Intended Audience :: Science/Research',
+               'Intended Audience :: Developers',
+               'License :: OSI Approved',
+               'Programming Language :: Python',
+               'Topic :: Software Development',
+               'Topic :: Scientific/Engineering',
+               'Operating System :: Microsoft :: Windows',
+               'Operating System :: POSIX',
+               'Operating System :: Unix',
+               'Operating System :: MacOS',
+               'Programming Language :: Python :: 3.5',
+               'Programming Language :: Python :: 3.6',
+               'Programming Language :: Python :: 3.7',
+	       'Programming Language :: Python :: 3.8']
+
+setup(
+    name="IoTPy", # Replace with your own username
     version="1.0.0",
     author="Mani Chandy",
     author_email="",
@@ -12,11 +32,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/AssembleSoftware/IoTPy",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+    classifiers= CLASSIFIERS,
+    packages = find_packages(),
+    install_requires = REQ
 )
+
