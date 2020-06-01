b770623260296d775f6485595213e3f03ecbcf1d
Kanianthra Mani Chandy <kmchandy@gmail.com>
2020-06-01 13:11:57 -0700

    used .

--- IoTPy/agent_types/check_agent_parameter_types.py
+++ IoTPy/agent_types/check_agent_parameter_types.py
@@ -1,10 +1,8 @@
 import types
 import inspect
 
-import sys
-sys.path.append("../core")
-from stream import Stream, StreamArray
-from agent import InList
+from ..core.stream import StreamArray, Stream 
+from ..core.agent import InList
 
 def check_list_of_streams_type(list_of_streams, agent_name, parameter_name):
     """ Helper function to check the types of streams used by an agent.
