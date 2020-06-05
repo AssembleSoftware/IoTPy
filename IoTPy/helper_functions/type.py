db2cb03497e5d99d00c81402fb1cc8cc2f487a79
Kanianthra Mani Chandy <kmchandy@gmail.com>
2020-06-05 11:37:22 -0700

    Converts multiprocessing.Array to StreamArray using dtype=float or int

---
+++ IoTPy/helper_functions/type.py
@@ -0,0 +1,22 @@
+from IoTPy.core.agent import Agent, InList
+from IoTPy.core.stream import StreamArray, Stream, _no_value, _multivalue
+from IoTPy.agent_types.op import map_element
+from IoTPy.helper_functions.recent_values import recent_values
+
+def dtype_float(stream):
+    s = StreamArray(dtype=float)
+    map_element(func=lambda v: float(v), in_stream=stream, out_stream=s)
+    return s
+
+def dtype_int(stream):
+    s = StreamArray(dtype=int)
+    map_element(func=lambda v: int(v), in_stream=stream, out_stream=s)
+    return s
+
+def dtype_double(stream):
+    s = StreamArray(dtype=double)
+    map_element(func=lambda v: double(v), in_stream=stream, out_stream=s)
+    return s
+
+
+
