8e0379c9fda8198b9ffb1fc40603d52d2c35c4e5
Kanianthra Mani Chandy <kmchandy@gmail.com>
2020-08-10 21:43:51 -0700

    Changed parallel constructs to fit the new way of specifying multiprocess.

--- examples/Twitter/twitter_parallel.py
+++ examples/Twitter/twitter_parallel.py
@@ -1,107 +1,100 @@
-import sys
-import os
-import re
-import tweepy
+#import re
+import pprint
 import json
 import threading
-
+import time
+# Tweepy is an open source free Python library for Twitter.
+# http://www.tweepy.org/
+import tweepy
+# The Natural Language ToolKit
+import nltk
+#nltk.download('punkt')
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+# Sentiment analysis
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-sys.path.append(os.path.abspath('../../IoTPy/agent_types'))
-sys.path.append(os.path.abspath('../../IoTPy/multiprocessing'))
-sys.path.append(os.path.abspath('../../IoTPy/core'))
-sys.path.append(os.path.abspath('../../IoTPy/helper_functions'))
-from stream import Stream
-from multicore import shared_memory_process, Multiprocess
-from sink import sink_element, stream_to_file
-from op import map_element
-from merge import zip_stream
-from twitter import twitter_to_stream, twitter_analysis
+import sys
+sys.path.append("../")
+from IoTPy.core.stream import Stream, run
+from IoTPy.agent_types.sink import sink_element, stream_to_file
+from IoTPy.agent_types.op import map_window, map_element
+from IoTPy.agent_types.merge import zip_stream
+from IoTPy.helper_functions.recent_values import recent_values
+from IoTPy.concurrency.multicore import get_processes
+from IoTPy.concurrency.multicore import get_processes_and_procs
+from IoTPy.concurrency.multicore import extend_stream, terminate_stream
+from IoTPy.helper_functions.print_stream import print_stream
 
-from twitter_sentiment_retweets import get_text
-from twitter_sentiment_retweets import followers_and_retweets_of_tweet
-from twitter_sentiment_retweets import sentiment_of_text
+from examples.Twitter.twitter import twitter_to_stream, twitter_analysis, check_Twitter_credentials
+from examples.Twitter.twitter_sentiment_retweets import get_text
+from examples.Twitter.twitter_sentiment_retweets import followers_and_retweets_of_tweet
+from examples.Twitter.twitter_sentiment_retweets import sentiment_of_text
 
-# Variables that contain the user credentials to access Twitter API 
-access_token = "999118734320009216-jaE4Rmc6fU11sMmBKb566YTFAJoMPV5"
-access_token_secret = "6ZxqJdK2RU6iridMX1MzSqr3uNpQsC9fv1E6otpZquLiF"
-consumer_key = "Iv6RTiO7Quw3ivH0GWPWqbiD4"
-consumer_secret = "theWmGwcKFG76OtTerxwhrxfX5nSDqGDWB2almLlp2ndRpxACm"
+#-----------------------------------------------------------------
+# Variables that contain the user credentials to access Twitter API
+# Create your own variables.
+access_token = ""
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
 
 def twitter_parallel(
         consumer_key, consumer_secret,
         access_token, access_token_secret,
         trackwords, num_steps):
-    
-    # PROCESS 0
-    def source(out_stream):
-        return twitter_to_stream(
-            consumer_key, consumer_secret,
-            access_token, access_token_secret,
-            trackwords, out_stream, num_steps)
-    def compute_func_0(in_streams, out_streams):
-        map_element(lambda x: x, in_stream=in_streams[0],
-                    out_stream=out_streams[0])
-    proc_0 = shared_memory_process(
-        compute_func=compute_func_0,
-        in_stream_names=['in'],
-        out_stream_names=['out'],
-        connect_sources=[('in', source)],
-        name='proc_0')
+    def copy_input_to_output(in_streams, out_streams):
+        map_element(lambda x: x, in_streams[0], out_streams[0])
 
-    # PROCESS 1
-    def compute_func_1(in_streams, out_streams):
+    def output_tweet_sentiment(in_streams, out_streams):
         def get_sentiment(tweet):
-            tweet_text = get_text(tweet)
-            sentiment_of_tweet = sentiment_of_text(tweet_text)     
-            return (tweet_text, sentiment_of_tweet)
+            return sentiment_of_text(get_text(tweet))
+        map_element(get_sentiment, in_streams[0], out_streams[0])
+        print_stream(out_streams[0], 'sentiment')
+    
+    def output_followers_retweets(in_streams, out_streams):
         map_element(
-            func=get_sentiment,
-            in_stream=in_streams[0],
-            out_stream=out_streams[0])
-    proc_1 = shared_memory_process(
-        compute_func=compute_func_1,
-        in_stream_names=['in'],
-        out_stream_names=['out'],
-        connect_sources=[],
-        name='proc_1')
+            followers_and_retweets_of_tweet, in_streams[0], out_streams[0])
+        print_stream(out_streams[0], 'followers')
 
-    # PROCESS 2
-    def compute_func_2(in_streams, out_streams):
-        map_element(
-            func=followers_and_retweets_of_tweet,
-            in_stream=in_streams[0],
-            out_stream=out_streams[0])
-    proc_2 = shared_memory_process(
-        compute_func=compute_func_2,
-        in_stream_names=['in'],
-        out_stream_names=['out'],
-        connect_sources=[],
-        name='proc_2')
-
-    # PROCESS 3
-    def compute_func_3(in_streams, out_streams):
+    def zip_everything_and_file(in_streams, out_streams):
         t = Stream()
         zip_stream(in_streams, out_stream=t)
+        print_stream(t, 'zipped')
         stream_to_file(in_stream=t, filename='result.dat')
-    proc_3 = shared_memory_process(
-        compute_func=compute_func_3,
-        in_stream_names=['in_1', 'in_2'],
-        out_stream_names=[],
-        connect_sources=[],
-        name='proc_3')
 
-    mp = Multiprocess(
-        processes=[proc_0, proc_1, proc_2, proc_3],
-        connections=[(proc_0, 'out', proc_1, 'in'),
-                     (proc_0, 'out', proc_2, 'in'),
-                     (proc_1, 'out', proc_3, 'in_1'),
-                     (proc_2, 'out', proc_3, 'in_2')])
-    mp.run()
+    multicore_specification = [
+        # Streams
+        [('source', 'x'), ('raw_tweets', 'x'),
+         ('tweet_sentiment', 'x'), ('followers_and_retweets', 'x')],
+        # Processes
+        [
+            {'name': 'get_raw_tweets', 'agent': copy_input_to_output,
+             'inputs':['source'], 'outputs':['raw_tweets'],
+            'sources':['source']},
+            {'name': 'get_tweet_sentiment', 'agent': output_tweet_sentiment,
+             'inputs':['raw_tweets'], 'outputs':['tweet_sentiment']},
+            {'name': 'get_followers_and_retweets', 'agent': output_followers_retweets,
+             'inputs':['raw_tweets'], 'outputs':['followers_and_retweets']},
+            {'name': 'put_results_in_file', 'agent': zip_everything_and_file,
+             'inputs':['tweet_sentiment', 'followers_and_retweets']}
+        ]
+    ]
+
+    # PROCESSES
+    processes, procs = get_processes_and_procs(multicore_specification)
+    stream_name = 'source'
+    get_tweets_thread = twitter_to_stream(
+        consumer_key, consumer_secret, access_token, access_token_secret,
+        trackwords, stream_name, procs, num_steps)
+    procs['get_raw_tweets'].threads = [get_tweets_thread]
+
+    for process in processes: process.start()
+    for process in processes: process.join()
+    for process in processes: process.terminate()
 
 if __name__ == '__main__':
     twitter_parallel(
         consumer_key, consumer_secret,
         access_token, access_token_secret,
-        trackwords=['Trump'], num_steps=10)
-    
+        trackwords=['Trump'], num_steps=3)
