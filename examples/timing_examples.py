dbbe03bfac58097ded7ab187bff8493649d46980
Kanianthra Mani Chandy <kmchandy@gmail.com>
2020-08-25 10:03:31 -0700

    Changed to dot notation for imports.

--- examples/timing_examples.py
+++ examples/timing_examples.py
@@ -1,16 +1,15 @@
 import sys
-import os
-sys.path.append(os.path.abspath("../../IoTPy/helper_functions"))
-sys.path.append(os.path.abspath("../../IoTPy/core"))
-sys.path.append(os.path.abspath("../../IoTPy/agent_types"))
-sys.path.append(os.path.abspath("../../IoTPy/concurrency"))
-from source import source_func_to_stream
-from multicore import multicore
-from multicore import run_single_process_single_source
-from multicore import copy_data_to_source, source_finished
-from sink import sink_element
-from print_stream import print_stream
+sys.path.append("../")
+from IoTPy.core.stream import Stream, run
+from IoTPy.agent_types.sink import sink_element
+from IoTPy.helper_functions.print_stream import print_stream
 
+from IoTPy.concurrency.multicore import get_processes, get_processes_and_procs
+from IoTPy.concurrency.multicore import terminate_stream
+from IoTPy.concurrency.multicore import get_proc_that_inputs_source
+from IoTPy.concurrency.multicore import extend_stream
+
+import threading
 import time
 import ntplib
 import statistics
@@ -85,86 +84,143 @@ list_of_ntp_servers =  [
     "3.us.pool.ntp.org"
     ]
 
-# This is the network of agents that operates on the data
-# acquired from the source. This network consists of a single
-# sink agent. Create the sink agent by using the encapsulator
-# sink_element to encapsulate terminating function
-# print_output.
-def h(in_streams, out_streams):
-    def print_output(v): print (v)
-    sink_element(func=print_output, in_stream=in_streams[0])
-
 
 def test_0():
-    #------------------------------------------------
-    # Test of ntp running in its own thread using
-    # run_single_process_single_source() instead of
-    # explicitly writing processes and connections.
-    #------------------------------------------------
+    # Implement single process with thread.
+    # Step 0: Define agent functions, source threads 
+    # and actuator threads (if any).
+
+    # Step 0.0: Define agent functions.
+    def printer_agent(in_streams, out_streams):
+        print_stream(in_streams[0])
+
+    # Step 0.1: Define source thread targets (if any).
     ntp_obj = ntp_single_server("0.us.pool.ntp.org")
-    def source_thread_target(source):
+
+    def source_thread_target(procs):
         num_steps=3
         for i in range(num_steps):
             v = ntp_obj.offset()
-            copy_data_to_source([v], source)
+            extend_stream(procs, data=[v], 
+                          stream_name='ntp')
             time.sleep(0.01)
-        source_finished(source)
-    def compute_func(in_streams, out_streams):
-        print_stream(in_streams[0])
-    run_single_process_single_source(source_thread_target, compute_func)
+        terminate_stream(procs, stream_name='ntp')
+
+    # Step 1: multicore_specification of streams and processes.
+    # Specify Streams: list of pairs (stream_name, stream_type).
+    # Specify Processes: name, agent function, 
+    #       lists of inputs and outputs, additional arguments.
+    multicore_specification = [
+        # Streams
+        [('ntp', 'f')],
+        # Processes
+        [{'name': 'p0', 'agent': printer_agent, 'inputs': ['ntp'],
+          'args' : [], 'sources': ['ntp']}]]
+
+    # Step 2: Create processes.
+    processes, procs = get_processes_and_procs(multicore_specification)
+
+    # Step 3: Create threads (if any)
+    thread_0 = threading.Thread(target=source_thread_target, args=(procs,))
+
+    # Step 4: Specify which process each thread runs in.
+    # thread_0 runs in the process called 'coordinator'
+    procs['p0'].threads = [thread_0]
+
+    # Step 5: Start, join and terminate processes.
+    for process in processes: process.start()
+    for process in processes: process.join()
+    for process in processes: process.terminate()
 
 
 def test_1():
+    # Step 0: Define agent functions, source threads 
+    # and actuator threads (if any).
+
+    # Step 0.0: Define agent functions.
+    def printer_agent(in_streams, out_streams):
+        print_stream(in_streams[0])
+
+    # Step 0.1: Define source thread targets (if any).
     servers = ntp_multiple_servers(list_of_ntp_servers)
-    def source_thread_target(source):
+    def source_thread_target(procs):
         num_steps=3
         for i in range(num_steps):
             v = servers.first_offset()
-            copy_data_to_source([v], source)
+            extend_stream(procs, data=[v], 
+                          stream_name='ntp')
             time.sleep(0.01)
-        source_finished(source)
-    def compute_func(in_streams, out_streams):
-        print_stream(in_streams[0])
-    run_single_process_single_source(source_thread_target, compute_func)
+        terminate_stream(procs, stream_name='ntp')
 
+    # Step 1: multicore_specification of streams and processes.
+    # Specify Streams: list of pairs (stream_name, stream_type).
+    # Specify Processes: name, agent function, 
+    #       lists of inputs and outputs, additional arguments.
+    multicore_specification = [
+        # Streams
+        [('ntp', 'f')],
+        # Processes
+        [{'name': 'p0', 'agent': printer_agent, 'inputs': ['ntp'], 'sources': ['ntp']}]]
+
+    # Step 2: Create processes.
+    processes, procs = get_processes_and_procs(multicore_specification)
+
+    # Step 3: Create threads (if any)
+    thread_0 = threading.Thread(target=source_thread_target, args=(procs,))
+
+    # Step 4: Specify which process each thread runs in.
+    # thread_0 runs in the process called 'coordinator'
+    procs['p0'].threads = [thread_0]
+
+    # Step 5: Start, join and terminate processes.
+    for process in processes: process.start()
+    for process in processes: process.join()
+    for process in processes: process.terminate()
+
+        
 def test_2():
+    # Step 0: Define agent functions, source threads 
+    # and actuator threads (if any).
+
+    # Step 0.0: Define agent functions.
+    def printer_agent(in_streams, out_streams):
+        print_stream(in_streams[0])
+
+    # Step 0.1: Define source thread targets (if any).
     def source_thread_target(source):
         servers = ntp_multiple_servers(list_of_ntp_servers)
-        num_steps=1
+        num_steps=3
         for i in range(num_steps):
             v = servers.time_and_first_offset()
-            copy_data_to_source([v], source)
+            extend_stream(procs, data=[v], 
+                          stream_name='ntp')
             time.sleep(0.01)
-        source_finished(source)
-    def compute_func(in_streams, out_streams):
-        print_stream(in_streams[0])
+        terminate_stream(procs, stream_name='ntp')
 
-    # Specify processes and connections.
-    processes = \
-      {
-        'process':
-           {'in_stream_names_types': [('in', 'x')],
-            'out_stream_names_types': [],
-            'compute_func': compute_func,
-            'sources':
-              {'ntp':
-                  {'type': 'x',
-                   'func': source_thread_target
-                  },
-               },
-            'actuators': {}
-           }
-      }
-    
-    connections = \
-      {
-          'process' :
-            {
-                'ntp' : [('process', 'in')]
-            }
-      }
+    # Step 1: multicore_specification of streams and processes.
+    # Specify Streams: list of pairs (stream_name, stream_type).
+    # Specify Processes: name, agent function, 
+    #       lists of inputs and outputs, additional arguments.
+    multicore_specification = [
+        # Streams
+        [('ntp', 'x')],
+        # Processes
+        [{'name': 'p0', 'agent': printer_agent, 'inputs': ['ntp'], 'sources': ['ntp']}]]
 
-    multicore(processes, connections)
+    # Step 2: Create processes.
+    processes, procs = get_processes_and_procs(multicore_specification)
+
+    # Step 3: Create threads (if any)
+    thread_0 = threading.Thread(target=source_thread_target, args=(procs,))
+
+    # Step 4: Specify which process each thread runs in.
+    # thread_0 runs in the process called 'coordinator'
+    procs['p0'].threads = [thread_0]
+
+    # Step 5: Start, join and terminate processes.
+    for process in processes: process.start()
+    for process in processes: process.join()
+    for process in processes: process.terminate()
 
 if __name__ == '__main__':
     print ('starting test_0')
