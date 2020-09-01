34819d4507462b7ddb4e22fac5369ddc34e133bd
Kanianthra Mani Chandy <kmchandy@gmail.com>
2020-08-31 21:05:09 -0700

    Dot notation.

--- examples/Counting/bloom_filter.py
+++ examples/Counting/bloom_filter.py
@@ -1,99 +1,76 @@
 """
-This code uses PyProbables:
-https://pyprobables.readthedocs.io/en/latest/index.html
-You will have to install PyProbables to use this code.
-
-This code is a straightforward application of the
-HeavyHitters class in PyProbables to create an agent with a
-single input stream and a single output stream. The
-heavy hitters in the input stream are placed on the
-output stream when ever the length of the input stream
-is a multiple of window_size (a parameter).
+Install PyProbables to use this code.
+See: https://pyprobables.readthedocs.io/en/latest/code.html#bloomfilter
+Straightforward use of BloomFilter.
 """
+from probables import BloomFilter
+
 import sys
-import os
+sys.path.append("../")
+from IoTPy.core.stream import Stream, _no_value, run
+from IoTPy.agent_types.op import map_element
+from IoTPy.helper_functions.print_stream import print_stream
 
-sys.path.append(os.path.abspath("../../IoTPy/core"))
-sys.path.append(os.path.abspath("../../IoTPy/multiprocessing"))
-sys.path.append(os.path.abspath("../../IoTPy/agent_types"))
-sys.path.append(os.path.abspath("../../IoTPy/helper_functions"))
-
-# stream is in ../../IoTPy/core
-from stream import Stream
-# op, source, sink are in ../../IoTPy/agent_types
-from op import map_element
-from source import source_file_to_stream
-from sink import stream_to_file
-# multicore is in ../../IoTPy/multiprocessing
-from multicore import run_single_process_single_source
-# helper_control is in ../../IoTPy/helper_functions
-from helper_control import _no_value
-
-import copy
-from probables import (BloomFilter)
-from probables.hashes import (default_sha256, default_md5)
-
-
-def bloom_filter_on_stream(in_stream, out_stream, blm):
+def bloom_filter_stream(in_stream, out_stream, bloom_filter):
     """
     Parameters
     ----------
        in_stream: Stream
           The input stream of the agent.
-          Each element of the input stream is a pair:
-          (function_name, string) where function_name is
-          a string which is one of 'add', 'check', 'remove'
+          Each element of the input stream is a pair which
+          is either ('add', object) or ('check', object).
+          If ('add', z) appears in the input stream then
+          z is added to the Bloom Filter set.
+          If ('check', z) appears then (z, 'True') appears
+          on the output stream if z is in the filter, and
+          (z, 'False') appears otherwise.
        out_stream: Stream
-          The output stream of the agent. An element is
-          added to the output stream when a 'check'
+          The output stream of the agent.
+          An element is added to the output stream when a 'check'
           function_name appears on the input stream.
-          The output stream consists of pairs (string, boolean)
-          where boolean is True if and only if string
+          The output stream consists of pairs (object, boolean)
+          where boolean is True if and only if object
           is in the input stream at this point.
-       blm: BloomFilter
-          An instance of BloomFilter.
+       bloom_filter: BloomFilter
+          An instance of the BloomFilter class.
 
     """
+    # The function for the map_element agent.
     def func(element):
         function_name, obj = element
         if function_name == 'add':
-            blm.add(obj)
-            return _no_value
-        elif function_name == 'remove':
-            blm.remove(obj)
+            bloom_filter.add(obj)
             return _no_value
         elif function_name == 'check':
-            obj_is_in_input_stream = blm.check(obj)
-            # True if obj is in the input stream at this point.
-            # False otherwise.
-            return (obj, obj_is_in_input_stream)
+            return (obj, bloom_filter.check(obj))
         else:
             raise ValueError
-        
+    # Create the map_element agent.
     map_element(func, in_stream, out_stream)
 
-def test_bloom_filter(in_filename, out_filename):
-    from probables.hashes import (default_sha256, default_md5)
-    
-    blm = BloomFilter(est_elements=1000, false_positive_rate=0.05,
-                      hash_function=default_sha256)
-    def compute_func(in_streams, out_streams):
-        y = Stream('Bloom output stream')
-        bloom_filter_on_stream(
-            in_stream=in_streams[0], out_stream=y, blm=blm)
-        stream_to_file(in_stream=y, filename=out_filename)
-    def source_func(out_stream):
-        def g(element):
-            function_name, obj = element.split()
-            return (function_name, obj)
-        return source_file_to_stream(
-            func=g, out_stream=out_stream, filename=in_filename)
-    run_single_process_single_source(source_func, compute_func)
+
+#---------------------------------------------------------------------------
+#       TEST
+#---------------------------------------------------------------------------
+def test():
+    bloom_filter = BloomFilter(est_elements=1000, false_positive_rate=0.05)
+
+    x = Stream('Bloom Filter input')
+    y = Stream('Bloom Filter output')
+    bloom_filter_stream(x, y, bloom_filter=bloom_filter)
+    print_stream(y, y.name)
+
+    # Run test data
+    data=[('add', 'a'), ('add', 'b'), ('add', 'a'),
+          ('check', 'c'), ('add', 'd'), ('check','a')]
+    x.extend(data)
+    run()
+
+    data=[('add', 'c'), ('check', 'b'), ('check', 'a'),
+          ('check', 'c'), ('check', 'e'), ('add', 'a')]
+    x.extend(data)
+    run()
 
 if __name__ == '__main__':
-    test_bloom_filter(in_filename='input_bloom.txt',
-                      out_filename='output_bloom.txt')
-
-    
-    
+    test()    
 
