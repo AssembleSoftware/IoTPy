17a4437153763d13c9cd0c9ee84b72c6f230d6f3
Kanianthra Mani Chandy <kmchandy@gmail.com>
2020-09-03 11:07:19 -0700

    Added code to handle '_finished' at the end of a list to signal termination.

--- tests/distributed/pika_subscriber_test.py
+++ tests/distributed/pika_subscriber_test.py
@@ -30,6 +30,8 @@ from IoTPy.concurrency.multicore import get_proc_that_inputs_source
 from IoTPy.concurrency.multicore import extend_stream
 from IoTPy.concurrency.PikaSubscriber import PikaSubscriber
 
+
+
 def pika_subscriber_test():
     """
     This function shows how to use Pika to receive a
@@ -52,10 +54,14 @@ def pika_subscriber_test():
     def pika_callback_thread_target(procs):
         def callback(ch, method, properties, body):
             data=json.loads(body)
-            print ('data[-1] is ', data[-1])
             if data[-1] == '_finished':
-                print ('terminating')
+                # Received a '_finished' message indicating that the 
+                # thread should terminate, and no further messages will
+                # be arriving on this stream.
+                # Extend the stream with data excluding'_finished'
+                extend_stream(procs, data[:-1], stream_name='pika_receive_stream')
                 terminate_stream(procs, stream_name='pika_receive_stream')
+                # Terminate the callback thread.
                 sys.exit()
             else:
                 extend_stream(procs, data, stream_name='pika_receive_stream')
@@ -92,6 +98,74 @@ def pika_subscriber_test():
     for process in processes: process.start()
     for process in processes: process.join()
     for process in processes: process.terminate()
+
+
+
+
+## def pika_subscriber_test():
+##     """
+##     This function shows how to use Pika to receive a
+##     stream that was sent using PikaPublisher.
+
+##     The pika_receive_agent prints its single input stream.
+##     The pika_callback_thread receives JSON input (see 'body
+##     in callback) from Pika and puts the data into a stream
+##     called 'pika_receive_stream'.
+
+##     """
+##     # Step 0: Define agent functions, source threads 
+##     # and actuator threads (if any).
+
+##     # Step 0.0: Define agent functions.
+##     def pika_receive_agent(in_streams, out_streams):
+##         print_stream(in_streams[0], 'x')
+
+##     # Step 0.1: Define source thread targets (if any).
+##     def pika_callback_thread_target(procs):
+##         def callback(ch, method, properties, body):
+##             data=json.loads(body)
+##             if data[-1] == '_finished':
+##                 # Received a '_finished' message indicating that the 
+##                 # thread should terminate, and no further messages will
+##                 # be arriving on this stream.
+##                 terminate_stream(procs, stream_name='pika_receive_stream')
+##                 # Terminate the callback thread.
+##                 sys.exit()
+##             else:
+##                 extend_stream(procs, data, stream_name='pika_receive_stream')
+##         # Declare the Pika subscriber
+##         pika_subscriber = PikaSubscriber(
+##             callback, routing_key='temperature',
+##             exchange='publications', host='localhost')
+##         pika_subscriber.start()
+
+##     # Step 1: multicore_specification of streams and processes.
+##     # Specify Streams: list of pairs (stream_name, stream_type).
+##     # Specify Processes: name, agent function, 
+##     #       lists of inputs and outputs, additional arguments.
+##     # The pika_receive_stream data type is 'x' for arbitrary.
+##     multicore_specification = [
+##         # Streams
+##         [('pika_receive_stream', 'x')],
+##         # Processes
+##         [{'name': 'pika_receive_process', 'agent': pika_receive_agent,
+##           'inputs':['pika_receive_stream'], 'sources': ['pika_receive_stream']}]]
+
+##     # Step 2: Create processes.
+##     processes, procs = get_processes_and_procs(multicore_specification)
+
+##     # Step 3: Create threads (if any)
+##     pika_callback_thread = threading.Thread(
+##         target=pika_callback_thread_target, args=(procs,))
+
+##     # Step 4: Specify which process each thread runs in.
+##     # pika_callback_thread runs in the process called 'pika_receive_process'
+##     procs['pika_receive_process'].threads = [pika_callback_thread]
+
+##     # Step 5: Start, join and terminate processes.
+##     for process in processes: process.start()
+##     for process in processes: process.join()
+##     for process in processes: process.terminate()
 
 if __name__ == '__main__':
     pika_subscriber_test()
