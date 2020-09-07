8e8cfa1b0e47bd45a79e3e78db35f20c191be7d0
Kanianthra Mani Chandy <kmchandy@gmail.com>
2020-09-07 15:39:19 -0700

    Added dot notation. Ran tests.

--- IoTPy/concurrency/PikaSubscriber.py
+++ IoTPy/concurrency/PikaSubscriber.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 import pika
+import json
 import threading
+import sys
+sys.path.append("../")
+
+# multicore imports
+from IoTPy.concurrency.multicore import get_processes_and_procs
+from IoTPy.concurrency.multicore import terminate_stream
+from IoTPy.concurrency.multicore import extend_stream
 
 class PikaSubscriber(object):
     def __init__(self, callback, routing_key,
@@ -23,5 +31,24 @@ class PikaSubscriber(object):
             queue=self.queue_name, on_message_callback=self.callback, auto_ack=True)
     def start(self):
         self.channel.start_consuming()
+
+
+def pika_callback_thread_target(procs, stream_name, routing_key, exchange, host):
+    def callback(ch, method, properties, body):
+        data=json.loads(body)
+        if data[-1] == '_finished':
+            # Received a '_finished' message indicating that the 
+            # thread should terminate, and no further messages will
+            # be arriving on this stream.
+            # Extend the stream with data excluding'_finished'
+            extend_stream(procs, data[:-1], stream_name)
+            terminate_stream(procs, stream_name)
+            # Terminate the callback thread.
+            sys.exit()
+        else:
+            extend_stream(procs, data, stream_name)
+    # Declare and start the Pika subscriber
+    pika_subscriber = PikaSubscriber(
+        callback, routing_key, exchange, host).start()
 
 
