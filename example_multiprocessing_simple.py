de8fc8b7f0b1311d8756d8dbe0c66da6d652232f
Mani Chandy <kmchandy@gmail.com>
2022-06-24 11:56:56 -0700

    Made file consistent with the Jupyter notebook example.

--- example_multiprocessing_simple.py
+++ example_multiprocessing_simple.py
@@ -1,11 +1,18 @@
 import multiprocessing as mp
 
+# import files containing target functions of 
+# processes x, y.
+# The target functions are in separate files because of
+# features of iPython and Jupyter notebooks.
 import example_multiprocessing_simple_process_x
 import example_multiprocessing_simple_process_y
 
 if __name__ == '__main__':
 
-    # dict_queues passed to processes x and y
+    # q_x is the input queue for process x
+    # q_y is the input queue for process y
+    # put the queues into a dict that is passed to processes x, y.
+    # Don't pass queues directly to external files.
     q_x, q_y = mp.Queue(), mp.Queue()
     dict_queues = {'x': q_x, 'y': q_y}
 
@@ -13,12 +20,14 @@ if __name__ == '__main__':
     process_x = mp.Process(
         target=\
         example_multiprocessing_simple_process_x.process_target_x,
-        args=(dict_queues,))
+        args=(dict_queues,),
+        name='process_x')
 
     process_y = mp.Process(
         target=\
         example_multiprocessing_simple_process_y.process_target_y,
-        args=(dict_queues,))
+        args=(dict_queues,),
+        name='process_y')
 
     # Start; join; terminate processes x and y
     process_x.daemon = True
